716e9a154d179aba6d1dc5f0461d49b809dc6024
ahmed khaled <ahmedelads789@gmail.com>
2024-01-17 18:42:51 +0200

    brithday_card

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+
+  runApp(BrithdayCard()
+  );
+
+}
+class BrithdayCard extends StatelessWidget {
+  BrithdayCard() ;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0XFFD2BCD5),
+        body: Center(
+          child: Image(
+            image: AssetImage("assets/2022-01-19_06-29-41-33803e677e5b58cfcf6c40e60220beb3.png"),
+          ),
+        ),
+      ),
+    ) ;
+  }
+}
